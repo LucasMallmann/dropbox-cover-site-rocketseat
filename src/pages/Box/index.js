306f840c9dfd30ae887e0657f90b475c297e36a3
Lucas Mallmann <lucasmallmann76@gmail.com>
2019-04-12 23:08:11 -0300

    Socket.io adiconado para adicionar arquivos RealTime

--- src/pages/Box/index.js
+++ src/pages/Box/index.js
@@ -3,6 +3,8 @@ import logo from "../../assets/logo.svg";
 import { MdInsertDriveFile } from "react-icons/md";
 import { distanceInWords } from "date-fns";
 import pt from "date-fns/locale/pt";
+import Dropzone from "react-dropzone";
+import socket from "socket.io-client";
 
 import "./style.css";
 import api from "../../services/api";
@@ -13,10 +15,37 @@ class Box extends Component {
   };
 
   async componentDidMount() {
+    this.subscribeToNewFiles();
     const box = this.props.match.params.id;
     const response = await api.get(`boxes/${box}`);
     this.setState({ box: response.data });
   }
+
+  subscribeToNewFiles = () => {
+    const box = this.props.match.params.id;
+    const io = socket("https://omnistack-backend-rocketseat.herokuapp.com");
+    io.emit("connectRoom", box);
+
+    io.on("file", data => {
+      this.setState({
+        box: { ...this.state.box, files: [data, ...this.state.box] }
+      });
+    });
+  };
+
+  /**
+   * @param {array} files
+   */
+  handleUpload = files => {
+    console.log(files);
+    files.forEach(file => {
+      const data = new FormData();
+      const box = this.props.match.params.id;
+
+      data.append("file", data);
+      api.post(`/boxes/${box}/files`, data);
+    });
+  };
 
   render() {
     return (
@@ -25,6 +54,15 @@ class Box extends Component {
           <img src={logo} alt="" />
           <h1>{this.state.box.title}</h1>
         </header>
+
+        <Dropzone onDropAccepted={this.handleUpload}>
+          {({ getRootProps, getInputProps }) => (
+            <div className="upload" {...getRootProps()}>
+              <input {...getInputProps()} />
+              <p>Arraste arquivos ou clique aqui</p>
+            </div>
+          )}
+        </Dropzone>
 
         <ul>
           {this.state.box.files &&
